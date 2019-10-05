22937a55ec24528ed17f56ed17431a7b31056bbe
Louis Bichard <contact@louisjohnbichard.co.uk>
2019-10-05 08:24:17 +0100

    Added provider

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+  version = "~> 2.0"
+  region  = "eu-west-1"
+}
+
 resource "aws_s3_bucket" "mys3bucket" {
   bucket = "terraform-refactoring-exercise-123123"
   acl    = "public-read"
